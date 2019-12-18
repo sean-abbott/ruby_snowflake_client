8fc96461ee9d251510dcebecd220cae8e5f4320c
Don Mitchell <dmitchell@cargurus.com>
2019-12-18 14:34:41 -0500

    Force compilation of dependencies

--- build_libsnowflake.sh
+++ build_libsnowflake.sh
@@ -3,7 +3,12 @@
 RUBY_SNOWFLAKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LIB_SNOWFLAKE_DIR="${LIB_SNOWFLAKE_DIR:-$RUBY_SNOWFLAKE_DIR/../libsnowflakeclient}"
 
-$LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
+# zlib static lib isn't compiled by build_dependencies and when linked later says it should have been compiled w fPIC
+source $LIB_SNOWFLAKE_DIR/deps/zlib-1.2.11/configure --static  # may add CFLAGS="-fPIC" but this looks like it does it
+source $LIB_SNOWFLAKE_DIR/deps/zlib-1.2.11/make
+
+source $LIB_SNOWFLAKE_DIR/scripts/build_dependencies.sh -t Release
+source $LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   PLATFORM="darwin"
