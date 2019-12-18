92b27314f3bc88cbd2064375eb4fb3e39917fa6e
Don Mitchell <dmitchell@cargurus.com>
2019-12-18 10:42:42 -0500

    Ensre lib dir exists

--- build_libsnowflake.sh
+++ build_libsnowflake.sh
@@ -3,6 +3,8 @@
 LIB_SNOWFLAKE_DIR = "${LIB_SNOWFLAKE_DIR:-/libsnowflakeclient}"
 
 $LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
-find $LIB_SNOWFLAKE_DIR -name *.a | xargs -I {} cp {} ./lib
-cp $LIB_SNOWFLAKE_DIR/include/snowflake/{client.h,logger.h,platform.h,basic_types.h,Simba_CRTFunctionSafe.h,version.h} ./lib
-cp $LIB_SNOWFLAKE_DIR/lib/cJSON.h ./lib
+
+mkdir -p LIB_SNOWFLAKE_DIR/lib
+find $LIB_SNOWFLAKE_DIR -name *.a | xargs -I {} cp {} $LIB_SNOWFLAKE_DIR/lib
+cp $LIB_SNOWFLAKE_DIR/include/snowflake/{client.h,logger.h,platform.h,basic_types.h,Simba_CRTFunctionSafe.h,version.h} $LIB_SNOWFLAKE_DIR/lib
+cp $LIB_SNOWFLAKE_DIR/lib/cJSON.h $LIB_SNOWFLAKE_DIR/lib
