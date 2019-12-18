6a026efd0c51e30d228d93140cee5990e163773a
Don Mitchell <dmitchell@cargurus.com>
2019-12-18 15:39:36 -0500

    debug script

--- build_libsnowflake.sh
+++ build_libsnowflake.sh
@@ -7,12 +7,12 @@ CURDIR=$PWD
 
 # zlib static lib isn't compiled by build_dependencies and when linked later says it should have been compiled w fPIC
 cd $LIB_SNOWFLAKE_DIR/deps/zlib-1.2.11  # zlib build gets confused if it's not pwd
-source configure --static  # may add CFLAGS="-fPIC" but this looks like it does it
-source make
+./configure --static  # may add CFLAGS="-fPIC" but this looks like it does it
+./make
 cd $CURDIR
 
 #source $LIB_SNOWFLAKE_DIR/scripts/build_dependencies.sh -t Release
-source $LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
+./$LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   PLATFORM="darwin"
