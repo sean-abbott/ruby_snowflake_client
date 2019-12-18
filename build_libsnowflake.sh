d9238e28e31944f7182632920ad8805fc4ab353c
Don Mitchell <dmitchell@cargurus.com>
2019-12-18 12:20:19 -0500

    Get libs from platform path

--- build_libsnowflake.sh
+++ build_libsnowflake.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# TODO fix the below to work if env var not set
-LIB_SNOWFLAKE_DIR = "${LIB_SNOWFLAKE_DIR:-/libsnowflakeclient}"
+
 RUBY_SNOWFLAKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LIB_SNOWFLAKE_DIR="${LIB_SNOWFLAKE_DIR:-$RUBY_SNOWFLAKE_DIR/../libsnowflakeclient}"
 
 $LIB_SNOWFLAKE_DIR/scripts/build_libsnowflakeclient.sh -s -t Release
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PLATFORM="darwin"
+else
+  PLATFORM="linux"
+fi
+
 mkdir -p $RUBY_SNOWFLAKE_DIR/lib
-find $LIB_SNOWFLAKE_DIR -name *.a | xargs -I {} cp {} $RUBY_SNOWFLAKE_DIR/lib
+cp $LIB_SNOWFLAKE_DIR/cmake-build/libsnowflakeclient.a $RUBY_SNOWFLAKE_DIR/lib
+find $LIB_SNOWFLAKE_DIR/deps-build/$PLATFORM -name *.a | xargs -I {} cp {} $RUBY_SNOWFLAKE_DIR/lib
 cp $LIB_SNOWFLAKE_DIR/include/snowflake/{client.h,logger.h,platform.h,basic_types.h,Simba_CRTFunctionSafe.h,version.h} $RUBY_SNOWFLAKE_DIR/lib
 cp $LIB_SNOWFLAKE_DIR/lib/cJSON.h $RUBY_SNOWFLAKE_DIR/lib
