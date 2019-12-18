7c2ac56e137c3415e52be04cb1bda46c618e042c
Don Mitchell <dmitchell@cargurus.com>
2019-12-18 15:21:49 -0500

    debug script

--- build_libsnowflake.sh
+++ build_libsnowflake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x # echo on for debugging
 
 RUBY_SNOWFLAKE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LIB_SNOWFLAKE_DIR="${LIB_SNOWFLAKE_DIR:-$RUBY_SNOWFLAKE_DIR/../libsnowflakeclient}"
@@ -24,3 +25,6 @@ cp $LIB_SNOWFLAKE_DIR/cmake-build/libsnowflakeclient.a $RUBY_SNOWFLAKE_DIR/lib
 find $LIB_SNOWFLAKE_DIR/deps-build/$PLATFORM -name *.a | xargs -I {} cp {} $RUBY_SNOWFLAKE_DIR/lib
 cp $LIB_SNOWFLAKE_DIR/include/snowflake/{client.h,logger.h,platform.h,basic_types.h,Simba_CRTFunctionSafe.h,version.h} $RUBY_SNOWFLAKE_DIR/lib
 cp $LIB_SNOWFLAKE_DIR/lib/cJSON.h $RUBY_SNOWFLAKE_DIR/lib
+
+# debuggin
+ls $RUBY_SNOWFLAKE_DIR/lib
